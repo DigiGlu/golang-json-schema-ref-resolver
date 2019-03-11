c2fba0f8ac8957388bff3f7a30038a2238d2a553
Dirk Rejahl <dirk@digiglu.co.uk>
2019-03-11 15:54:57 +0000

    .

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -3,7 +3,7 @@ package resolver
 import "fmt"
 
 // DeReference ist just another Hello World
-func DeReference(inJSON []byte) {
+func Dereference(inJSON []byte) {
 	// This is just for testing
 	fmt.Printf("hello, world\n")
 }
